a477681cf7b751d0c9db4d640e3f38989994471a
AddieEli <acadavid71@gmail.com>
2017-08-31 18:41:38 -0500

    had to remove thermometer code - frownface

--- app/controllers/hives_controller.rb
+++ app/controllers/hives_controller.rb
@@ -54,7 +54,7 @@ before_action :authenticate_user!
 
     @hive.geocode
 
-    # @internal = Unirest.get(URI.decode("https://api.particle.io/v1/devices/380032000651353530373132/hive_tmp?access_token=a6d70792fd8cbe5a00d67b2c28dc85264a12457e")).body["result"]
+    
     
 
 
